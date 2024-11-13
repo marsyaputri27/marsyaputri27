7a28a073b354b48d1becc343526f7c2f1bcbd5b1
marsyaputri27 <marsyaputri086@gmail.com>
2024-11-14 06:49:59 +0700

    Add files via upload

---
+++ p5/dasbord.js
@@ -0,0 +1,25 @@
+
+
+// function ig() {
+//     window.location.href = "https://www.instagram.com/foerda61_?igsh=ZTA5ZmVpaHI0eG9s";
+// }
+
+// function dasbord() { 
+//     window.location.href = "dasbord.html";
+// }
+
+// function showAlert() {
+//     // Memeriksa apakah alert sudah ditampilkan sebelumnya
+//     if (!localStorage.getItem('alertShown')) {
+//         alert('Selamat datang di SMK NEGERI 1 DENPASAR!'); // Tampilkan alert
+//         localStorage.setItem('alertShown', 'true'); // Simpan status alert
+//     }
+// }
+
+// // Panggil fungsi showAlert saat halaman dimuat
+// window.onload = function() {
+//     showAlert();
+// };
+
+
+
---
+++ p5/data.js
@@ -0,0 +1,114 @@
+function showInfo() {
+    var container = document.querySelector("#info-container-sanis")
+    container.style.display='block';
+}
+
+function showInfo2() {
+    var container = document.querySelector("#info-container-dayu")
+    container.style.display='block';
+}
+
+function showInfo3() {
+    var container = document.querySelector("#info-container-alit")
+    container.style.display='block';
+}
+
+function showInfo4() {
+    var container = document.querySelector("#info-container-agung")
+    container.style.display='block';
+}
+
+function showInfo5() {
+    var container = document.querySelector("#info-container-dangin")
+    container.style.display='block';
+}
+
+function showInfo6() {
+    var container = document.querySelector("#info-container-ngurah")
+    container.style.display='block';
+}
+
+function showInfo7() {
+    var container = document.querySelector("#info-container-yana")
+    container.style.display='block';
+}
+
+
+function closeInfo() {
+    var container = document.querySelector("#info-container-sanis")
+    container.style.display='none';
+}
+
+
+function closeInfo2() {
+    var container = document.querySelector("#info-container-dayu")
+    container.style.display='none';
+}
+
+
+function closeInfo3() {
+    var container = document.querySelector("#info-container-alit")
+    container.style.display='none';
+}
+
+
+function closeInfo4() {
+    var container = document.querySelector("#info-container-agung")
+    container.style.display='none';
+}
+
+
+function closeInfo5() {
+    var container = document.querySelector("#info-container-dangin")
+    container.style.display='none';
+}
+
+
+function closeInfo6() {
+    var container = document.querySelector("#info-container-ngurah")
+    container.style.display='none';
+}
+
+
+
+function closeInfo7() {
+    var container = document.querySelector("#info-container-yana")
+    container.style.display='none';
+}
+
+function normatif() {
+    window.open("aset/Guru normatif.xlsx","_blank");
+}
+
+function pesan(){
+    window.open("https://wa.me/6281329866458", "_blank");
+}
+
+function pesan2(){
+    window.open("https://wa.me/6281936392557", "_blank");
+}
+
+function pesan3(){
+    window.open("https://wa.me/62818552418", "_blank");
+}
+
+function pesan4(){
+    window.open("https://wa.me/6285737426996", "_blank");
+}
+
+function pesan5(){
+    window.open("https://wa.me/6281328640667", "_blank");
+}
+
+function pesan6(){
+    window.open("https://wa.me/6285792211194", "_blank");
+}
+
+function pesan7(){
+    window.open("https://wa.me/6289671138300", "_blank");
+}
+
+
+function showDetail() {
+    window.open("aset/TAV.xlsx","_blank");
+}
